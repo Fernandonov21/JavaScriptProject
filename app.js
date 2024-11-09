e455ad84548ec636d1d18c1b21b10458c9c9ebae
Fernandonov21 <fernandonovillo21@gmail.com>
2024-11-09 12:24:17 -0500

    feat: :sparkles: New Hello World with Node.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // app.js
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
 
+// Configurar la ruta principal
+app.get('/', (req, res) => {
+    res.send('Hello, World!');  // Enviar "Hello, World!" como respuesta
+});
+
+// Iniciar el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
+});
